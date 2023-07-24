d8f7673b9d4dfd710280c5425f43637d033411cc
Janaka Peiris <janaka.peiris@chamonix.com.au>
2023-07-24 12:39:50 +0930

    Add project files.

---
+++ DataAccessLibrary/PeopleData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLibrary.Models;
+
+namespace DataAccessLibrary
+{
+    public class PeopleData
+    {
+        private readonly ISQLDataAccess _db;
+
+        public PeopleData(ISQLDataAccess db)
+        {
+            this._db = db;
+        }
+
+        public  Task<List<PersonModel>> GetPeople()
+        {
+            string sql = "select * from dbo.Poeple";
+
+        }
+    }
+}
---
+++ DataAccessLibrary/SQLDataAccess.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+
+namespace DataAccessLibrary
+{
+    public class SQLDataAccess : ISQLDataAccess
+    {
+        private readonly IConfiguration _config;
+
+        public string ConnectionStringName { get; set; } = "Default";
+
+        public SQLDataAccess(IConfiguration config)
+        {
+            this._config = config;
+        }
+
+        public async Task<List<T>> LoadData<T, U>(string sql, U parameters)
+        {
+            string connectionString = _config.GetConnectionString(ConnectionStringName);
+
+            using (IDbConnection conn = new SqlConnection(connectionString))
+            {
+                var data = await conn.QueryAsync<T>(sql, parameters);
+                return data.ToList();
+
+            }
+        }
+
+        public async Task SaveData<T>(string sql, T parameters)
+        {
+            string connectionString = _config.GetConnectionString(ConnectionStringName);
+
+            using (IDbConnection conn = new SqlConnection(connectionString))
+            {
+                await conn.ExecuteAsync(sql, parameters);
+
+            }
+        }
+    }
+}
